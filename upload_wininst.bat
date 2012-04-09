8e326215876c9ff36d449a80c2ad8aeb589249cb
Matt Newville <newville@cars.uchicago.edu>
2012-04-09 11:53:47 -0500

    added install, change py3.1 to py3.2

--- upload_wininst.bat
+++ upload_wininst.bat
@@ -1,13 +1,18 @@
 REM
 REM  %HOME%\.pypirc must be setup with PyPI info.
 REM
+SET HOME=M:\
 SET PATH=C:\Python26;%PATH%
+python setup.py install
 python setup.py bdist_wininst --target-version=2.6 upload
 
 SET PATH=C:\Python27;%PATH%
+python setup.py install
 python setup.py bdist_wininst --target-version=2.7 upload
 
-SET PATH=C:\Python31;%PATH%
-python setup.py bdist_wininst --target-version=3.1 upload
+SET PATH=C:\Python32;%PATH%
+python setup.py install
+python setup.py bdist_wininst --target-version=3.2 upload
+
 
 
