91624a85df60c40d6f416e4d45b039fb2d1693d3
Till Stensitzki <mail.till@gmx.de>
2012-04-20 19:15:56 +0200

    added more options and better error handling

--- lib/diagnostics.py
+++ lib/diagnostics.py
@@ -15,26 +15,33 @@ def f_compare(N,P,new_chi,best_chi):
     return f.cdf((new_chi/best_chi-1)*(N-P),1,N-P)
 
 
-def copy_vals(params, tmp_params):
+def copy_vals(params):
     "Saves the values of params in temporay dict"
+    tmp_params={}
     for para_key in params:
         tmp_params[para_key]=params[para_key].value
-
+    return tmp_params
+    
 def restore_vals(tmp_params,params):
     "Restores the values of params from a temporay dict"
     for para_key in params:
         params[para_key].value=tmp_params[para_key]
 
-def calc_ci(minimizer, maxiter=20, verbose=1):
-    "Calculates coinfidance intervals using the model comparison method."
+def calc_ci(minimizer, maxiter=200, verbose=1, 
+            prob_func=f_compare, sigmas=[0.674,0.95,0.997],
+            trace_params=False):
+    "Calculates coinfidance intervals using the "
     fit_params=minimizer.params
     #copy the best fit values.
-    org={}
-    copy_vals(fit_params,org)
+    if trace_params: 
+        trace={}
+    org=copy_vals(fit_params)
     output=[]
     best_chi=minimizer.chisqr
     
     for para in fit_params.values():        
+        p_trace=[]
+        trace[para.name]=p_trace
         if verbose:
             print 'Calculating CI for '+ para.name
         restore_vals(org,fit_params)
@@ -43,14 +50,19 @@ def calc_ci(minimizer, maxiter=20, verbose=1):
         start_val=para.value
         #minimizer.leastsq()                
         
-        def prob_func(val, offset=0.):
+        def calc_prob(val, offset=0.,restore=False):
             "Returns the probabilty for given Value."
+            if restore: restore_vals(org,fit_params)       
             para.value=val
             minimizer.prepare_fit(para)
             minimizer.leastsq()
-            out=minimizer
+            out=minimizer            
+                
             #print calc_max_chi(out.ndata, out.ndata-out.nfree,best_chi)
             prob=f_compare(out.ndata,out.ndata-out.nfree,out.chisqr,best_chi)                    
+            if trace_params:
+                p=list(copy_vals(out.params)).append(prob)
+                p_trace.append(p)
             return prob-offset
                         
         def search_limits(direction):
@@ -58,21 +70,26 @@ def calc_ci(minimizer, maxiter=20, verbose=1):
             Searchs for the limits. First it looks for a upper limit and 
             then finds the sigma-limits with help of scipy root finder.
             """ 
-            prob=0
+            change=0
+            old_prob=0
             i=0
             limit=start_val
             #Find a upper limit,
-            while prob<0.999:
+            while change>0.001:
                 i+=1
                 limit+=step*direction
-                prob=prob_func(limit)
+                new_prob=calc_prob(limit)
+                change=new_prob-old_prob
+                old_prob=new_prob
                 if i>maxiter:
+                    print "Reached maxiter, last val: ", limit, " with ", old_prob
                     break
-                
+                print change, limit, old_prob
             restore_vals(org,fit_params)
-            #use brentq to find sigmas.
-            sigmas=[0.674,0.95,0.997]
-            ret = [brentq(prob_func,start_val,limit, args=(p)) for p in sigmas]
+            #use brentq to find sigmas.            
+            
+            ret = [brentq(calc_prob,start_val,limit, args=(p)) 
+                    for p in sigmas if p<old_prob]
             return ret
         
         
