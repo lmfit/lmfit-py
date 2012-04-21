c4188ccf4ec6feca0207534ce11a1932868e3402
Till Stensitzki <mail.till@gmx.de>
2012-04-21 02:05:38 +0200

    added calc_2dmap, changed f_compare to allow different number of fixed varibales (nfix).

--- lib/diagnostics.py
+++ lib/diagnostics.py
@@ -10,23 +10,26 @@ def calc_max_chi(N,P,best_chi):
     fval=f.isf(0.05,P,N-P)
     return best_chi*(fval*P/float(N-P)+1)
 
-def f_compare(N,P,new_chi,best_chi):
-    """Returns the probalitiy for two given parameter sets"""  
+def f_compare(N,P,new_chi,best_chi,nfix=1.):
+    """
+    Returns the probalitiy for two given parameter sets.
+    nfix is the number of fixed parameters.   
+    """  
     #print new_chi, best_chi, N, P
-    return f.cdf((new_chi/best_chi-1)*(N-P),1,N-P)
-
+    P=P+nfix
+    return f.cdf((new_chi/best_chi-1)*(N-P)/nfix,nfix,N-P)
 
 def copy_vals(params):
-    "Saves the values of params in temporay dict"
+    "Saves the values of paras and errs in temporay dict"
     tmp_params={}
     for para_key in params:
-        tmp_params[para_key]=params[para_key].value
+        tmp_params[para_key]=(params[para_key].value,params[para_key].stderr)
     return tmp_params
     
 def restore_vals(tmp_params,params):
     "Restores the values of params from a temporay dict"
     for para_key in params:
-        params[para_key].value=tmp_params[para_key]
+        params[para_key].value, params[para_key].stderr=tmp_params[para_key]
         
 def p_trace_to_dict(p_tr,params):
     """
@@ -117,11 +120,52 @@ def calc_ci(minimizer, maxiter=200, verbose=1,
         upper_err=search_limits(1)
         restore_vals(org,fit_params)        
         lower_err=search_limits(-1)
-        trace[para.name]=p_trace_to_dict(p_trace,fit_params)
+        if trace_params:
+            trace[para.name]=p_trace_to_dict(p_trace,fit_params)
         para.vary=True          
         output.append([para.name]+list(lower_err[::-1])+[start_val]+list(upper_err))
-    
+   
     restore_vals(org,fit_params)
     if trace_params: 
            return output, trace
     return output
+    
+
+def calc_2dmap(minimizer,x_name,y_name,nx=10,ny=10):
+    best_chi=minimizer.chisqr
+    org=copy_vals(minimizer.params)
+    
+    x=minimizer.params[x_name]
+    x_upper, x_lower=x.value+3*x.stderr, x.value-3*x.stderr
+    x_points=np.linspace(x_lower,x_upper,nx)
+    
+    y=minimizer.params[y_name]
+    y_upper, y_lower=y.value+3*y.stderr, y.value-3*y.stderr
+    y_points=np.linspace(y_lower,y_upper,ny)
+
+    x.vary=False
+    y.vary=False
+    grid=np.dstack(np.meshgrid(x_points,y_points))
+
+    
+    def calc_prob(vals, offset=0.,restore=False):
+            "Returns the probabilty for given Value."
+            if restore: restore_vals(org,minimizer.params)       
+            x.value=vals[0]
+            y.value=vals[1]
+            #minimizer.__prepared=False
+            minimizer.prepare_fit([x,y])
+            minimizer.leastsq()
+            out=minimizer            
+            
+            #print "calc"
+            #print calc_max_chi(out.ndata, out.ndata-out.nfree,best_chi)
+            prob=f_compare(out.ndata,out.ndata-out.nfree,out.chisqr,best_chi,
+                           nfix=2.)    
+            return prob
+    out=x_points, y_points, np.apply_along_axis(calc_prob,-1,grid)
+    
+    x.vary, y.vary=True, True
+    restore_vals(org, minimizer.params)
+    minimizer.chisqr=best_chi
+    return out
