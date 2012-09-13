d3b4ac421a6708f9aea2b9c210935ae359951de6
Matt Newville <newville@cars.uchicago.edu>
2012-09-13 10:45:15 -0500

    added several tests

---
+++ tests/fit_NIST_leastsq.py
@@ -0,0 +1,149 @@
+# from __future__ import print_function
+import sys
+import math
+
+try:
+    import matplotlib
+    matplotlib.use('WXAgg')
+    import pylab
+    HASPYLAB = True
+except ImportError:
+    HASPYLAB = False
+
+from scipy.optimize import leastsq, curve_fit
+from testutils import report_errors
+
+from NISTModels import Models, ReadNistData
+
+
+def ndig(a, b):
+    return int(0.5-math.log10(abs(abs(a)-abs(b))/abs(b)))
+
+def Compare_NIST_Results(DataSet, params, NISTdata):
+    print(' ======================================')
+    print(' %s: ' % DataSet)
+    print(' | Parameter Name |  Value Found   |  Certified Value | # Matching Digits |')
+    print( ' |----------------+----------------+------------------+-------------------|')
+
+    val_dig_min = 1000
+    err_dig_min = 1000
+    for i in range(NISTdata['nparams']):
+        parname = 'b%i' % (i+1)
+        par = params[parname]
+        thisval = par.value
+        certval = NISTdata['cert_values'][i]
+
+        thiserr = par.stderr
+        certerr = NISTdata['cert_stderr'][i]
+        vdig   = ndig(thisval, certval)
+        edig   = ndig(thiserr, certerr)
+
+        pname = (parname + ' value ' + ' '*14)[:14]
+        ename = (parname + ' stderr' + ' '*14)[:14]
+        print(' | %s | % -.7e | % -.7e   | %2i                |' % (pname, thisval, certval, vdig))
+        print(' | %s | % -.7e | % -.7e   | %2i                |' % (ename, thiserr, certerr, edig))
+
+        val_dig_min = min(val_dig_min, vdig)
+        err_dig_min = min(err_dig_min, edig)
+
+    print(' |----------------+----------------+------------------+-------------------|')
+    sumsq = NISTdata['sum_squares']
+    chi2 = 'xx' # myfit.chisqr
+    print(' | Sum of Squares | %.7e  | %.7e    |  %2i               |' % (chi2, sumsq,
+                                                                          ndig(chi2, sumsq)))
+    print(' |----------------+----------------+------------------+-------------------|')
+
+    print(' Worst agreement: %i digits for value, %i digits for error ' % (val_dig_min, err_dig_min))
+
+    return val_dig_min
+
+def NIST_Test(DataSet, start='start2', plot=True):
+
+    NISTdata = ReadNistData(DataSet)
+    resid, npar, dimx = Models[DataSet]
+    y = NISTdata['y']
+    x = NISTdata['x']
+
+    params = []
+    param_names = []
+    for i in range(npar):
+        pname = 'b%i' % (i+1)
+        cval  = NISTdata['cert_values'][i]
+        cerr  = NISTdata['cert_stderr'][i]
+        pval1 = NISTdata[start][i]
+        params.append(pval1)
+        param_names.append(pname)
+
+
+        #     myfit = Minimizer(resid, params, fcn_args=(x,), fcn_kws={'y':y},
+        #                       scale_covar=True)
+        # 
+    print 'lsout ', params
+    lsout = leastsq(resid, params, args=(x, y), full_output=True)
+
+    print 'lsout ', lsout
+    print params   , len(x), len(y)
+
+    digs = Compare_NIST_Results(DataSet,  params, NISTdata)
+
+    if plot and HASPYLAB:
+        fit = -resid(params, x, )
+        pylab.plot(x, y, 'r+-')
+        pylab.plot(x, fit, 'ko--')
+        pylab.show()
+
+    return digs > 2
+
+msg1 = """
+----- NIST StRD Models -----
+Select one of the Models listed below:
+and a starting point of 'start1' or 'start2'
+"""
+
+msg2 = """
+That is, use
+    python fit_NIST.py Bennet5 start1
+or go through all models and starting points with:
+    python fit_NIST.py all
+"""
+
+if __name__  == '__main__':
+    dset = 'Bennett5'
+    start = 'start2'
+    if len(sys.argv) < 2:
+        print(msg1)
+        out = ''
+        for d in sorted(Models.keys()):
+            out = out + ' %s ' % d
+            if len(out) > 55:
+                print( out)
+                out = ''
+        print(out)
+        print(msg2)
+       
+        sys.exit()
+
+    if len(sys.argv) > 1:
+        dset = sys.argv[1]
+    if len(sys.argv) > 2:
+        start = sys.argv[2]
+    if dset.lower() == 'all':
+        tpass = 0
+        tfail = 0
+        failures = []
+        dsets = sorted(Models.keys())
+        for dset in dsets:
+            for start in ('start1', 'start2'):
+                if NIST_Test(dset, start=start, plot=False):
+                    tpass += 1
+                else:
+                    tfail += 1
+                    failures.append("   %s (starting at '%s')" % (dset, start))
+
+        print('--------------------------------------')
+        print(' Final Results: %i pass, %i fail.' % (tpass, tfail))
+        print(' Tests Failed for:\n %s' % '\n '.join(failures))
+        print('--------------------------------------')
+    else:
+        NIST_Test(dset, start=start, plot=True)
+
---
+++ tests/fit_pvoigt2.py
@@ -0,0 +1,87 @@
+import sys
+
+from numpy import linspace, zeros, sin, exp, random, sqrt, pi, sign
+
+from lmfit import Parameters, Parameter, Minimizer
+from lmfit.utilfuncs import gauss, loren, pvoigt
+from lmfit.printfuncs import report_errors
+
+try:
+    import matplotlib
+    matplotlib.use('WXAGG')
+    import pylab
+    HASPYLAB = True
+except ImportError:
+    HASPYLAB = False
+
+
+def residual(pars, x, sigma=None, data=None):
+    yg = gauss(x, pars['amp_g'].value,
+               pars['cen_g'].value, pars['wid_g'].value)
+    yl = loren(x, pars['amp_l'].value,
+               pars['cen_l'].value, pars['wid_l'].value)
+
+    slope = pars['line_slope'].value
+    offset = pars['line_off'].value
+    model =  yg +  yl + offset + x * slope
+    if data is None:
+        return model
+    if sigma is  None:
+        return (model - data)
+    return (model - data)/sigma
+
+
+n = 601
+xmin = 0.
+xmax = 20.0
+x = linspace(xmin, xmax, n)
+
+data = (gauss(x, 21, 8.1, 1.2) + 
+        loren(x, 10, 9.6, 2.4) +
+        random.normal(scale=0.23,  size=n) +
+        x*0.5)
+
+
+if HASPYLAB:
+    pylab.plot(x, data, 'r+')
+
+pfit = [Parameter(name='amp_g',  value=10),
+        Parameter(name='cen_g',  value=9),
+        Parameter(name='wid_g',  value=1),
+
+        Parameter(name='amp_tot',  value=20),
+        Parameter(name='amp_l',  expr='amp_tot - amp_g'),
+        Parameter(name='cen_l',  expr='1.5+cen_g'),
+        Parameter(name='wid_l',  expr='2*wid_g'),
+        
+        Parameter(name='line_slope', value=0.0),
+        Parameter(name='line_off', value=0.0)]
+
+sigma = 0.021  # estimate of data error (for all data points)
+
+myfit = Minimizer(residual, pfit, 
+                  fcn_args=(x,), fcn_kws={'sigma':sigma, 'data':data},
+                  scale_covar=True)
+
+myfit.prepare_fit()
+init = residual(myfit.params, x)
+
+if HASPYLAB:
+    pylab.plot(x, init, 'b--')
+
+myfit.leastsq()
+
+print(' Nfev = ', myfit.nfev)
+print( myfit.chisqr, myfit.redchi, myfit.nfree)
+
+report_errors(myfit.params)
+
+fit = residual(myfit.params, x)
+
+if HASPYLAB:
+    pylab.plot(x, fit, 'k-')
+    pylab.show()
+
+
+
+
