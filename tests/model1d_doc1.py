313d0039819835fb79fc3bfa89482ef843e6af5c
Matt Newville <newville@cars.uchicago.edu>
2013-11-29 22:21:09 -0600

    simplifed model1d example

--- tests/model1d_doc1.py
+++ tests/model1d_doc1.py
@@ -5,20 +5,15 @@ import matplotlib.pyplot as plt
 data = np.loadtxt('model1d_gauss.dat')
 x = data[:, 0]
 y = data[:, 1]
- 
+
 model = GaussianModel()
-
-model.guess_starting_values(y, x)
-
-init_fit = model.model(x=x)
 
 model.fit(y, x=x)
 
-print model.fit_report()
+print model.fit_report(min_correl=0.25)
 
 final_fit = model.model(x=x)
 
+plt.plot(x, final_fit, 'r-')
 plt.plot(x, y,         'bo')
-plt.plot(x, init_fit,  'k--')
-plt.plot(x, final_fit, 'r')
 plt.show()
