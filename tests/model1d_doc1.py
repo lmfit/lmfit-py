66b9da587c90d08554f795eb8d73cea0214ecf63
Matt Newville <newville@cars.uchicago.edu>
2013-12-01 10:58:28 -0600

    updated tests

--- tests/model1d_doc1.py
+++ tests/model1d_doc1.py
@@ -7,7 +7,10 @@ x = data[:, 0]
 y = data[:, 1]
 
 model = GaussianModel()
+model.guess_starting_values(y, x=x)
+# model.params['amplitude'].value=6.0
 
+init_fit = model.model(x=x)
 model.fit(y, x=x)
 
 print model.fit_report(min_correl=0.25)
@@ -15,5 +18,6 @@ print model.fit_report(min_correl=0.25)
 final_fit = model.model(x=x)
 
 plt.plot(x, final_fit, 'r-')
+plt.plot(x, init_fit, 'k--')
 plt.plot(x, y,         'bo')
 plt.show()
