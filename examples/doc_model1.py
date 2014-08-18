808e3c9706fdd48ec3cf78c2e1df17b159418641
Matt Newville <newville@cars.uchicago.edu>
2014-08-17 22:33:29 -0500

    update doc example files

---
+++ examples/doc_model1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+#<examples/doc_model1.py>
+from numpy import sqrt, pi, exp, linspace, loadtxt
+from lmfit import fit_report, Model
+
+import matplotlib.pyplot as plt
+
+data = loadtxt('model1d_gauss.dat')
+x = data[:, 0]
+y = data[:, 1]
+
+def gaussian(x, amp, cen, wid):
+    "1-d gaussian: gaussian(x, amp, cen, wid)"
+    return (amp/(sqrt(2*pi)*wid)) * exp(-(x-cen)**2 /(2*wid**2))
+
+gmod = Model(gaussian)
+result = gmod.fit(y, x=x, amp=5, cen=5, wid=1)
+
+print(fit_report(result.params))
+
+plt.plot(x, y,         'bo')
+plt.plot(x, result.init_fit, 'k--')
+plt.plot(x, result.best_fit, 'r-')
+plt.show()
+#<end examples/doc_model1.py>
