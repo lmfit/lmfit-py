1b24745107595fb134da431c006e8b44eb30ba4a
Matt Newville <newville@cars.uchicago.edu>
2014-08-21 19:37:43 -0500

    move pngmath/mathjax option to imported extensions.py, copied in Makefile

---
+++ doc/sphinx/ext_mathjax.py
@@ -0,0 +1,10 @@
+# sphinx extensions for mathjax
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.todo',
+              'sphinx.ext.coverage',
+              'sphinx.ext.intersphinx',
+              'numpydoc']
+mathjax = 'sphinx.ext.mathjax'
+pngmath = 'sphinx.ext.pngmath'
+
+extensions.append(mathjax)
