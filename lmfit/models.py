763b723428a819351a1f987c4f493840b1acf888
Matt Newville <newville@cars.uchicago.edu>
2014-09-01 09:14:00 -0500

    update models to use new Model class

--- lmfit/models.py
+++ lmfit/models.py
@@ -23,8 +23,9 @@ def index_of(arr, val):
         return 0
     return np.abs(arr-val).argmin()
 
-def estimate_peak(y, x, negative):
-    "estimate amp, cen, sigma for a peak"
+def guess_from_peak(model, y, x, negative,
+                    ampscale=1.0, sigscale=1.0, with_fwhm=False):
+    "estimate amp, cen, sigma for a peak, create params"
     if x is None:
         return 1.0, 0.0, 1.0
     maxy, miny = max(y), min(y)
@@ -42,7 +43,24 @@ def estimate_peak(y, x, negative):
     if len(halfmax_vals) > 2:
         sig = (x[halfmax_vals[-1]] - x[halfmax_vals[0]])/2.0
         cen = x[halfmax_vals].mean()
-    return amp*sig, cen, sig
+    amp = amp*sig*ampscale
+    sig = sig*sigscale
+
+    pars = model.make_params(amplitude=amp, center=cen, sigma=sig)
+    pars['%ssigma' % model.prefix].set(min=0.0)
+    if with_fwhm:
+        pars.add('%sfwhm' % model.prefix,
+                 expr='%.7f*%ssigma' % (model.fwhm_factor, model.prefix))
+    return pars
+
+def update_param_vals(pars, prefix, **kwargs):
+    """convenience function to update parameter values
+    with keyword arguments"""
+    for key, val in kwargs.items():
+        pname = "%s%s" % (prefix, key)
+        if pname in pars:
+            pars[pname].value = val
+    return pars
 
 COMMON_DOC = """
 
@@ -62,40 +80,40 @@ suffix: string to append to paramter names, needed to add two Models that
 class ConstantModel(Model):
     __doc__ = "x -> c" + COMMON_DOC
     def __init__(self, **kwargs):
-        def func(x, c):
+        def constant(x, c):
             return c
-        super(ConstantModel, self).__init__(func, **kwargs)
+        super(ConstantModel, self).__init__(constant, **kwargs)
 
-    def guess_starting_values(self, data, **kwargs):
-        self.set_param('c', data.mean())
-        self.has_initial_guess = True
+    def guess(self, data, **kwargs):
+        pars = self.make_params()
+        pars['%sc' % self.prefix].set(value=data.mean())
+        return update_param_vals(pars, self.prefix, **kwargs)
+
 
 class LinearModel(Model):
     __doc__ = linear.__doc__ + COMMON_DOC
     def __init__(self, **kwargs):
         super(LinearModel, self).__init__(linear, **kwargs)
 
-    def guess_starting_values(self, data, x=None, **kwargs):
+    def guess(self, data, x=None, **kwargs):
         sval, oval = 0., 0.
         if x is not None:
             sval, oval = np.polyfit(x, data, 1)
-        self.set_param('intercept', oval)
-        self.set_param('slope' , sval)
-        self.has_initial_guess = True
+        pars = self.make_params(intercept=oval, slope=sval)
+        return update_param_vals(pars, self.prefix, **kwargs)
+
 
 class QuadraticModel(Model):
     __doc__ = parabolic.__doc__ + COMMON_DOC
     def __init__(self, **kwargs):
         super(QuadraticModel, self).__init__(parabolic, **kwargs)
 
-    def guess_starting_values(self, data, x=None, **kwargs):
+    def guess(self, data, x=None, **kwargs):
         a, b, c = 0., 0., 0.
         if x is not None:
             a, b, c = np.polyfit(x, data, 2)
-        self.set_param('a', a)
-        self.set_param('b', b)
-        self.set_param('c', c)
-        self.has_initial_guess = True
+        pars = self.make_params(a=a, b=b, c=c)
+        return update_param_vals(pars, self.prefix, **kwargs)
 
 ParabolicModel = QuadraticModel
 
@@ -116,77 +134,62 @@ class PolynomialModel(Model):
 
         super(PolynomialModel, self).__init__(polynomial, **kwargs)
 
-    def guess_starting_values(self, data, x=None, **kws):
-        coefs = np.zeros(self.MAX_DEGREE+1)
+    def guess(self, data, x=None, **kwargs):
+        pars = self.make_params()
         if x is not None:
             out = np.polyfit(x, data, self.poly_degree)
             for i, coef in enumerate(out[::-1]):
-                coefs[i] = coef
-        for i in range(self.poly_degree+1):
-            self.set_param('c%i' % (i), coefs[i])
-        self.has_initial_guess = True
+                pars['%sc%i'% (self.prefix, i)].set(value=coef)
+        return update_param_vals(pars, self.prefix, **kwargs)
+
 
 class GaussianModel(Model):
     __doc__ = gaussian.__doc__ + COMMON_DOC
     fwhm_factor = 2.354820
     def __init__(self, **kwargs):
         super(GaussianModel, self).__init__(gaussian, **kwargs)
-        self.params.add('%sfwhm' % self.prefix,
-                        expr='%.6f*%ssigma' % (self.fwhm_factor, self.prefix))
 
-    def guess_starting_values(self, data, x=None, negative=False, **kwargs):
-        amp, cen, sig = estimate_peak(data, x, negative)
-        self.set_param('amplitude', amp)
-        self.set_param('center', cen)
-        self.set_param('sigma', sig, min=0.0)
-        self.has_initial_guess = True
+    def guess(self, data, x=None, negative=False, **kwargs):
+        pars = guess_from_peak(self, data, x, negative, with_fwhm=True)
+        return update_param_vals(pars, self.prefix, **kwargs)
+
 
 class LorentzianModel(Model):
     __doc__ = lorentzian.__doc__ + COMMON_DOC
     fwhm_factor = 2.0
     def __init__(self, **kwargs):
         super(LorentzianModel, self).__init__(lorentzian, **kwargs)
-        self.params.add('%sfwhm' % self.prefix,
-                        expr='%.7f*%ssigma' % (self.fwhm_factor,
-                                               self.prefix))
 
-    def guess_starting_values(self, data, x=None, negative=False, **kwargs):
-        amp, cen, sig = estimate_peak(data, x, negative)
-        self.set_param('amplitude', 1.25*amp)
-        self.set_param('center', cen)
-        self.set_param('sigma', sig, min=0.0)
-        self.has_initial_guess = True
+    def guess(self, data, x=None, negative=False, **kwargs):
+        pars = guess_from_peak(self, data, x, negative,
+                               ampscale=1.25, with_fwhm=True)
+        return update_param_vals(pars, self.prefix, **kwargs)
+
 
 class VoigtModel(Model):
     __doc__ = voigt.__doc__ + COMMON_DOC
     fwhm_factor = 3.60131
     def __init__(self, **kwargs):
         super(VoigtModel, self).__init__(voigt, **kwargs)
-        self.params.add('%sfwhm' % self.prefix,
-                        expr='%.7f*%ssigma' % (self.fwhm_factor,
-                                               self.prefix))
 
-    def guess_starting_values(self, data, x=None, negative=False, **kwargs):
-        amp, cen, sig = estimate_peak(data, x, negative)
-        self.set_param('amplitude', 1.5*amp)
-        self.set_param('center', cen)
-        self.set_param('sigma', 0.65*sig, min=0.0)
-        self.params['%sgamma' % self.prefix] = \
-                              Parameter('%sgamma' % self.prefix, expr= '%ssigma' % self.prefix)
-        self.has_initial_guess = True
+
+    def guess(self, data, x=None, negative=False, **kwargs):
+        pars = guess_from_peak(self, data, x, negative,
+                               ampscale=1.5, sigscale=0.65,
+                               with_fwhm=True)
+        pars.add('%sgamma' % self.prefix, expr='%ssigma' % self.prefix)
+        return update_param_vals(pars, self.prefix, **kwargs)
+
 
 class PseudoVoigtModel(Model):
     __doc__ = pvoigt.__doc__ + COMMON_DOC
     def __init__(self, **kwargs):
         super(PseudoVoigtModel, self).__init__(pvoigt, **kwargs)
 
-    def guess_starting_values(self, data, x=None, negative=False, **kwargs):
-        amp, cen, sig = estimate_peak(data, x, negative)
-        self.set_param('amplitude', 1.25*amp)
-        self.set_param('center', cen)
-        self.set_param('sigma', sig, min=0.0)
-        self.set_param('fraction', 0.5)
-        self.has_initial_guess = True
+    def guess(self, data, x=None, negative=False, **kwargs):
+        pars = guess_from_peak(self, data, x, negative, ampscale=1.25)
+        pars['%sfraction' % self.prefix].set(value=0.5)
+        return update_param_vals(pars, self.prefix, **kwargs)
 
 
 class Pearson7Model(Model):
@@ -194,13 +197,10 @@ class Pearson7Model(Model):
     def __init__(self, **kwargs):
         super(Pearson7Model, self).__init__(pearson7, **kwargs)
 
-    def guess_starting_values(self, data, x=None, negative=False, **kwargs):
-        amp, cen, sig = estimate_peak(data, x, negative)
-        self.set_param('amplitude', amp)
-        self.set_param('center', cen)
-        self.set_param('sigma',  sig, min=0.0)
-        self.set_param('expon',  1.5)
-        self.has_initial_guess = True
+    def guess(self, data, x=None, negative=False, **kwargs):
+        pars = guess_from_peak(self, data, x, negative)
+        pars['%sexpon' % self.prefix].set(value=1.5)
+        return update_param_vals(pars, self.prefix, **kwargs)
 
 
 class StudentsTModel(Model):
@@ -208,60 +208,51 @@ class StudentsTModel(Model):
     def __init__(self, **kwargs):
         super(StudentsTModel, self).__init__(students_t, **kwargs)
 
-    def guess_starting_values(self, data, x=None, negative=False, **kwargs):
-        amp, cen, sig = estimate_peak(data, x, negative)
-        self.set_param('amplitude',  amp)
-        self.set_param('center', cen)
-        self.set_param('sigma', sig, min=0.0)
-        self.has_initial_guess = True
+    def guess(self, data, x=None, negative=False, **kwargs):
+        pars = guess_from_peak(self, data, x, negative)
+        return update_param_vals(pars, self.prefix, **kwargs)
+
 
 class BreitWignerModel(Model):
     __doc__ = breit_wigner.__doc__ + COMMON_DOC
     def __init__(self, **kwargs):
         super(BreitWignerModel, self).__init__(breit_wigner, **kwargs)
 
-    def guess_starting_values(self, data, x=None, negative=False, **kwargs):
-        amp, cen, sig = estimate_peak(data, x, negative)
-        self.set_param('amplitude', amp)
-        self.set_param('center', cen)
-        self.set_param('sigma', sig, min=0.0)
-        self.set_param('q', 1.0)
-        self.has_initial_guess = True
+    def guess(self, data, x=None, negative=False, **kwargs):
+        pars = guess_from_peak(self, data, x, negative)
+        pars['%sq' % self.prefix].set(value=1.0)
+        return update_param_vals(pars, self.prefix, **kwargs)
+
 
 class LognormalModel(Model):
     __doc__ = lognormal.__doc__ + COMMON_DOC
     def __init__(self, **kwargs):
         super(LognormalModel, self).__init__(lognormal, **kwargs)
 
-    def guess_starting_values(self, data, x=None, negative=False, **kwargs):
-        self.set_param('amplitude', 1.0)
-        self.set_param('center', 0.0)
-        self.set_param('sigma', 0.25, min=0.0)
-        self.has_initial_guess = True
+    def guess(self, data, x=None, negative=False, **kwargs):
+        pars = self.make_params(amplitude=1.0, center=0.0, sigma=0.25)
+        pars['%ssigma' % self.prefix].set(min=0.0)
+        return update_param_vals(pars, self.prefix, **kwargs)
+
 
 class DampedOscillatorModel(Model):
     __doc__ = damped_oscillator.__doc__ + COMMON_DOC
     def __init__(self, **kwargs):
         super(DampedOscillatorModel, self).__init__(damped_oscillator, **kwargs)
 
-    def guess_starting_values(self, data, x=None, negative=False, **kwargs):
-        amp, cen, sig = estimate_peak(data, x, negative)
-        self.set_param('amplitude', 0.1*amp)
-        self.set_param('center', cen)
-        self.set_param('sigma', 0.1*sig, min=0.0)
-        self.has_initial_guess = True
+    def guess(self, data, x=None, negative=False, **kwargs):
+        pars =guess_from_peak(self, data, x, negative,
+                              ampscale=0.1, sigscale=0.1)
+        return update_param_vals(pars, self.prefix, **kwargs)
 
 class ExponentialGaussianModel(Model):
     __doc__ = expgaussian.__doc__ + COMMON_DOC
     def __init__(self, **kwargs):
         super(ExponentialGaussianModel, self).__init__(expgaussian, **kwargs)
 
-    def guess_starting_values(self, data, x=None, negative=False, **kwargs):
-        amp, cen, sig = estimate_peak(data, x, negative)
-        self.set_param('amplitude', amp)
-        self.set_param('center', cen)
-        self.set_param('sigma', sig, min=0.0)
-        self.has_initial_guess = True
+    def guess(self, data, x=None, negative=False, **kwargs):
+        pars = guess_from_peak(self, data, x, negative)
+        return update_param_vals(pars, self.prefix, **kwargs)
 
 
 class DonaichModel(Model):
@@ -269,12 +260,9 @@ class DonaichModel(Model):
     def __init__(self, **kwargs):
         super(DonaichModel, self).__init__(donaich, **kwargs)
 
-    def guess_starting_values(self, data, x=None, negative=False, **kwargs):
-        amp, cen, sig = estimate_peak(data, x, negative)
-        self.set_param('amplitude', amp/2.0)
-        self.set_param('center', cen)
-        self.set_param('sigma', sig, min=0.0)
-        self.has_initial_guess = True
+    def guess(self, data, x=None, negative=False, **kwargs):
+        pars = guess_from_peak(self, data, x, negative, ampscale=0.5)
+        return update_param_vals(pars, self.prefix, **kwargs)
 
 
 class PowerLawModel(Model):
@@ -282,43 +270,45 @@ class PowerLawModel(Model):
     def __init__(self, **kwargs):
         super(PowerLawModel, self).__init__(powerlaw, **kwargs)
 
-    def guess_starting_values(self, data, x=None, **kws):
+    def guess(self, data, x=None, **kwargs):
         try:
             expon, amp = np.polyfit(log(x+1.e-14), log(data+1.e-14), 1)
         except:
             expon, amp = 1, np.log(abs(max(data)+1.e-9))
-        self.set_param('amplitude', np.exp(amp))
-        self.set_param('exponent', expon)
-        self.has_initial_guess = True
+
+        pars = self.make_params(amplitude=np.exp(amp), exponent=expon)
+        return update_param_vals(pars, self.prefix, **kwargs)
+
 
 class ExponentialModel(Model):
     __doc__ = exponential.__doc__ + COMMON_DOC
     def __init__(self, **kwargs):
         super(ExponentialModel, self).__init__(exponential, **kwargs)
 
-    def guess_starting_values(self, data, x=None, **kws):
+    def guess(self, data, x=None, **kwargs):
         try:
             sval, oval = np.polyfit(x, np.log(abs(data)+1.e-15), 1)
         except:
             sval, oval = 1., np.log(abs(max(data)+1.e-9))
-        self.set_param('amplitude', np.exp(oval))
-        self.set_param('decay', -1/sval)
-        self.has_initial_guess = True
+        pars = self.make_params(amplitude=np.exp(oval), decay=-1.0/sval)
+        return update_param_vals(pars, self.prefix, **kwargs)
+
 
 class StepModel(Model):
     __doc__ = step.__doc__ + COMMON_DOC
     def __init__(self, **kwargs):
         super(StepModel, self).__init__(step, **kwargs)
 
-    def guess_starting_values(self, data, x=None, **kws):
+    def guess(self, data, x=None, **kwargs):
         if x is None:
             return
         ymin, ymax = min(data), max(data)
         xmin, xmax = min(x), max(x)
-        self.set_param('amplitude', (ymax-ymin))
-        self.set_param('center',    (xmax+xmin)/2.0)
-        self.set_param('sigma',     (xmax-xmin)/7.0, min=0.0)
-        self.has_initial_guess = True
+        pars = self.make_params(amplitude=(ymax-ymin),
+                                center=(xmax+xmin)/2.0)
+        pars['%ssigma' % self.prefix].set(value=(xmax-xmin)/7.0, min=0.0)
+        return update_param_vals(pars, self.prefix, **kwargs)
+
 
 class RectangleModel(Model):
     __doc__ = rectangle.__doc__ + COMMON_DOC
@@ -327,15 +317,15 @@ class RectangleModel(Model):
         self.params.add('%smidpoint' % self.prefix,
                         expr='(%scenter1+%scenter2)/2.0' % (self.prefix,
                                                             self.prefix))
-    def guess_starting_values(self, data, x=None, **kws):
+    def guess(self, data, x=None, **kwargs):
         if x is None:
             return
         ymin, ymax = min(data), max(data)
         xmin, xmax = min(x), max(x)
-        self.set_param('amplitude', (ymax-ymin))
-        self.set_param('center1',   (xmax+xmin)/4.0)
-        self.set_param('sigma1' ,   (xmax-xmin)/7.0, min=0.0)
-        self.set_param('center2', 3*(xmax+xmin)/4.0)
-        self.set_param('sigma2',    (xmax-xmin)/7.0, min=0.0)
-        self.has_initial_guess = True
+        pars = self.make_params(amplitude=(ymax-ymin),
+                                center1=(xmax+xmin)/4.0,
+                                center2=3*(xmax+xmin)/4.0)
+        pars['%ssigma1' % self.prefix].set(value=(xmax-xmin)/7.0, min=0.0)
+        pars['%ssigma2' % self.prefix].set(value=(xmax-xmin)/7.0, min=0.0)
+        return update_param_vals(pars, self.prefix, **kwargs)
 
