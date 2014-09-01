8379ba78d67d4e7e1b1e887f9813a3ced08eb45a
Matt Newville <newville@cars.uchicago.edu>
2014-09-01 09:13:01 -0500

    working model with no params

--- lmfit/model.py
+++ lmfit/model.py
@@ -4,9 +4,9 @@ Concise nonlinear curve fitting.
 
 import warnings
 import inspect
-import copy
+from copy import deepcopy
 import numpy as np
-from . import Parameters, Parameter, minimize
+from . import Parameters, Minimizer
 from .printfuncs import fit_report
 
 # Use pandas.isnull for aligning missing data is pandas is available.
@@ -63,32 +63,49 @@ class Model(object):
     _invalid_ivar  = "Invalid independent variable name ('%s') for function %s"
     _invalid_par   = "Invalid parameter name ('%s') for function %s"
     _invalid_missing = "missing must be None, 'none', 'drop', or 'raise'."
+    _names_collide = "Two models have parameters named %s. Use distinct names"
+
     def __init__(self, func, independent_vars=None, param_names=None,
-                 missing='none', prefix='', components=None, name=None, **kws):
+                 missing='none', prefix='', name=None, **kws):
         self.func = func
         self.prefix = prefix
         self.param_names = param_names
         self.independent_vars = independent_vars
         self.func_allargs = []
         self.func_haskeywords = False
-        self.has_initial_guess = False
-        self.components = components
+        self._others = []
         if not missing in [None, 'none', 'drop', 'raise']:
             raise ValueError(self._invalid_missing)
         self.missing = missing
         self.opts = kws
         self.result = None
-        self.params = Parameters()
         self._parse_params()
-        self._residual = self._build_residual()
         if self.independent_vars is None:
             self.independent_vars = []
-        if name is None:
+        if name is None and hasattr(self.func, '__name__'):
             name = self.func.__name__
         self.name = name
 
+    def _reprstring(self, long=False):
+        buff, opts = [], []
+        if len(self.prefix) > 0:
+            opts.append("prefix='%s'" % (self.prefix))
+        if long:
+            for k, v in self.opts.items():
+                opts.append("%s='%s'" % (k, v))
+
+        out = "%s" % self.name
+        if len(opts) > 0:
+            out = "%s(%s)" % (out, ','.join(opts))
+        buff.append(out)
+        for other in self._others:
+            buff.extend(other._reprstring(long=long))
+        return buff
+
     def __repr__(self):
-        return  "<lmfit.Model: %s>" % (self.name)
+        buff = self._reprstring()
+        out = "+".join(buff)
+        return  "<lmfit.Model: %s>" % (out)
 
     def _parse_params(self):
         "build params from function arguments"
@@ -113,14 +130,14 @@ class Model(object):
 
         # default param names: all positional args
         # except independent variables
-        def_vals = {}
+        self.def_vals = {}
         if self.param_names is None:
             self.param_names = pos_args[:]
             for key, val in kw_args.items():
                 if (not isinstance(val, bool) and
                     isinstance(val, (float, int))):
                     self.param_names.append(key)
-                    def_vals[key] = val
+                    self.def_vals[key] = val
             for p in self.independent_vars:
                 if p in self.param_names:
                     self.param_names.remove(p)
@@ -141,47 +158,37 @@ class Model(object):
         for pname in self.param_names:
             names.append("%s%s" % (self.prefix, pname))
         self.param_names = set(names)
-        for name in self.param_names:
-            self.params.add(name)
-        for key, val in def_vals.items():
-            self.set_param(key, val)
 
-    def guess_starting_values(self, data=None, **kws):
+    def make_params(self, **kwargs):
+        pars = Parameters()
+        for name in self.param_names:
+            pars.add(name)
+            basename = name
+            if self.prefix is not None:
+                basename = name[len(self.prefix):]
+            if basename in self.def_vals:
+                pars[name].value = self.def_vals[basename]
+            if basename in kwargs:
+                pars[name].value = kwargs[basename]
+        for other in self._others:
+            pars.update(other.make_params(**kwargs))
+        return pars
+
+    def guess(self, data=None, **kws):
         """stub for guess starting values --
-        should be implemented for each model subclass
-        """
+        should be implemented for each model subclass to
+        run self.make_params(), update starting values
+        and return a Parameters object"""
         cname = self.__class__.__name__
-        msg = 'guess_starting_values() not implemented for %s' % cname
+        msg = 'guess() not implemented for %s' % cname
         raise NotImplementedError(msg)
 
-    def _build_residual(self):
-        "Generate and return a residual function."
-        def residual(params, data, weights, **kwargs):
-            "default residual:  (data-model)*weights"
-            diff = self.eval(params=params, **kwargs) - data
-            if weights is not None:
-                diff *= weights
-            return np.asarray(diff)  # for compatibility with pandas.Series
-        return residual
-
-    def make_funcargs(self, params, kwargs):
-        """convert parameter values and keywords to function arguments"""
-        out = {}
-        out.update(self.opts)
-        npref = len(self.prefix)
-        for name, par in params.items():
-            if npref > 0 and name.startswith(self.prefix):
-                name = name[npref:]
-            if name in self.func_allargs or self.func_haskeywords:
-                out[name] = par.value
-        for name, val in kwargs.items():
-            if name in self.func_allargs or self.func_haskeywords:
-                out[name] = val
-                if name in params:
-                    params[name].value = val
-        if self.func_haskeywords and self.components is not None:
-            out['__components__'] = self.components
-        return out
+    def _residual(self, params, data, weights, **kwargs):
+        "default residual:  (data-model)*weights"
+        diff = self.eval(params, **kwargs) - data
+        if weights is not None:
+            diff *= weights
+        return np.asarray(diff)  # for compatibility with pandas.Series
 
     def _handle_missing(self, data):
         "handle missing data"
@@ -195,44 +202,56 @@ class Model(object):
             mask = np.asarray(mask)  # for compatibility with pandas.Series
             return mask
 
-    def set_param(self, paramname, value,
-                  min=None, max=None, vary=True, expr=None):
-        """set parameter value and optionally the other attributes
-        (min/max/expr/vary),
+    def make_funcargs(self, params, kwargs, strip=True):
+        """convert parameter values and keywords to function arguments"""
+        out = {}
+        out.update(self.opts)
+        npref = len(self.prefix)
+        def strip_prefix(name):
+            if strip and npref > 0 and name.startswith(self.prefix):
+                name = name[npref:]
+            return name
 
-        this is useful for making and initial guess of parameter value.
+        for name, par in params.items():
+            name = strip_prefix(name)
+            if name in self.func_allargs or self.func_haskeywords:
+                out[name] = par.value
 
-        The name can include prefix or not
-        """
-        pname = paramname
-        if pname not in self.params and len(self.prefix) > 0:
-            pname = "%s%s" % (self.prefix, pname)
-        if pname not in self.params:
-            raise KeyError("'%s' not a parameter name" % pname)
-        self.params[pname].value = value
-        self.params[pname].vary = vary
-        if min is not None:
-            self.params[pname].min = min
-        if max is not None:
-            self.params[pname].max = max
-        if expr is not None:
-            self.params[pname].expr= expr
+        # kwargs handled slightly differently -- may set param value too!
+        for name, val in kwargs.items():
+            name = strip_prefix(name)
+            if name in self.func_allargs or self.func_haskeywords:
+                out[name] = val
+                if name in params:
+                    params[name].value = val
+        return out
 
-    def eval(self, params=None, **kwargs):
-        """evaluate the model with the supplied or current parameters"""
-        if params is None:
-            params = self.params
+    def _make_all_args(self, params, **kwargs):
+        """generate **all** function args for all functions"""
+        args = {}
+        for key, val in self.make_funcargs(params, kwargs).items():
+            args["%s%s" % (self.prefix, key)] = val
+        for other in self._others:
+            otherargs = other._make_all_args(params, **kwargs)
+            args.update(otherargs)
+        return args
+
+    def eval(self, params, **kwargs):
+        """evaluate the model with the supplied parameters"""
         fcnargs = self.make_funcargs(params, kwargs)
-        return self.func(**fcnargs)
+        result = self.func(**fcnargs)
+        for other in self._others:
+            result += other.eval(params, **kwargs)
+        return result
 
-    def fit(self, data, params=None, weights=None, method='leastsq',
+    def fit(self, data, params, weights=None, method='leastsq',
             iter_cb=None, scale_covar=True, **kwargs):
         """Fit the model to the data.
 
         Parameters
         ----------
         data: array-like
-        params: Parameters object, optional
+        params: Parameters object
         weights: array-like of same size as data
             used for weighted fit
         method: fitting method to use (default = 'leastsq')
@@ -243,7 +262,7 @@ class Model(object):
 
         Returns
         -------
-        lmfit.Minimizer
+        lmfit.ModelFitResult
 
         Examples
         --------
@@ -265,10 +284,7 @@ class Model(object):
         Parameter objects are unchanged.
 
         """
-        if params is None:
-            params = self.params
-        else:
-            params = copy.deepcopy(params)
+        params = deepcopy(params)
 
         # If any kwargs match parameter names, override params.
         param_kwargs = set(kwargs.keys()) & self.param_names
@@ -276,13 +292,10 @@ class Model(object):
             p = kwargs[name]
             if isinstance(p, Parameter):
                 p.name = name  # allows N=Parameter(value=5) with implicit name
-                params[name] = copy.deepcopy(p)
+                params[name] = deepcopy(p)
             else:
-                params[name] = Parameter(name=name, value=p)
+                params[name].set(value=p)
             del kwargs[name]
-
-        # Keep a pristine copy of the initial params.
-        init_params = copy.deepcopy(params)
 
         # All remaining kwargs should correspond to independent variables.
         for name in kwargs.keys():
@@ -300,7 +313,6 @@ class Model(object):
             raise ValueError("""Assign each parameter an initial value by
  passing Parameters or keyword arguments to fit""")
 
-
         # Handle null/missing values.
         mask = None
         if self.missing not in (None, 'none'):
@@ -316,75 +328,99 @@ class Model(object):
             if not np.isscalar(self.independent_vars):  # just in case
                 kwargs[var] = _align(kwargs[var], mask, data)
 
-        kwargs['__components__'] = self.components
-        result = minimize(self._residual, params, args=(data, weights),
-                          method=method, iter_cb=iter_cb,
-                          scale_covar=scale_covar, kws=kwargs)
-
-        # Monkey-patch the Minimizer object with some extra information.
-        result.model = self
-        result.init_params = init_params
-        result.init_values = self.make_funcargs(init_params, {})
-        if '__components__' in result.init_values:
-            result.init_values.pop('__components__')
-        result.init_fit = self.eval(params=init_params, **kwargs)
-        result.best_fit = self.eval(params=result.params, **kwargs)
-        self.result = result
-        return result
-
-    def fit_report(self, modelpars=None, show_correl=True, min_correl=0.1):
-        "return fit report"
-        if self.result is None:
-            raise ValueError("must run .fit() first")
-
-        return fit_report(self.result, modelpars=modelpars,
-                          show_correl=show_correl,
-                          min_correl=min_correl)
+        output = ModelFitResult(self, params, method=method, iter_cb=iter_cb,
+                                scale_covar=scale_covar, fcn_kws=kwargs)
+        output.fit(data=data, weight=weights)
+        return output
 
     def __add__(self, other):
         colliding_param_names = self.param_names & other.param_names
         if len(colliding_param_names) != 0:
             collision = colliding_param_names.pop()
-            raise NameError("Both models have parameters called " +
-                            "%s. Redefine the models " % collision +
-                            "with distinct names.")
+            raise NameError(_names_collide % collision)
 
-        def composite_func(**kwargs):
-            "composite model function"
-            components = kwargs.get('__components__', None)
-            out = None
-            if components is not None:
-                for comp in components:
-                    pars = Parameters()
-                    prefix = comp.prefix
-                    for p in self.params.values():
-                        if p.name.startswith(prefix):
-                            pars.__setitem__(p.name, p)
-                            pars[p.name].value = kwargs[p.name]
+        new = deepcopy(self)
+        new._others.append(other)
+        return new
 
-                    fcnargs = comp.make_funcargs(pars, kwargs)
-                    comp_out = comp.func(**fcnargs)
-                    if out is None:
-                        out = np.zeros_like(comp_out)
-                    out += comp_out
-            return out
 
-        components = self.components
-        if components is None:
-            components = [self]
-        if other.components is None:
-            components.append(other)
-        else:
-            components.extend(other.components)
-        all_params = self.params
-        for key, val in other.params.items():
-            all_params[key] = val
+class ModelFitResult(Minimizer):
+    """Result from Model fit
 
-        out = Model(func=composite_func, independent_vars=self.independent_vars,
-                    param_names=self.param_names | other.param_names,
-                    missing=self.missing, components=components)
-        out.components = components
-        for c in components:
-            c.params = self.params
-        out.params = all_params
+    Attributes
+    -----------
+    model         instance of Model -- the model function
+    params        instance of Parameters -- the fit parameters
+    data          array of data values to compare to model
+    weights       array of weights used in fitting
+    init_params   copy of params, before being updated by fit()
+    init_values   array of parameter values, before being updated by fit()
+    init_fit      model evaluated with init_params.
+    best_fit      model evaluated with params after being updated by fit()
+
+    Methods:
+    --------
+    fit(data=None, params=None, weights=None, method=None, **kwargs)
+         fit (or re-fit) model with params to data (with weights)
+         using supplied method.  The keyword arguments are sent to
+         as keyword arguments to the model function.
+
+         all inputs are optional, defaulting to the value used in
+         the previous fit.  This allows easily changing data or
+         parameter settings, or both.
+
+    eval(**kwargs)
+         evaluate the current model, with the current parameter values,
+         with values in kwargs sent to the model function.
+
+   fit_report(modelpars=None, show_correl=True, min_correl=0.1)
+         return a fit report.
+
+    """
+    def __init__(self, model, params, data=None, weights=None,
+                 method='leastsq', fcn_args=None, fcn_kws=None,
+                 iter_cb=None, scale_covar=True, **fit_kws):
+        self.model = model
+        self.data = data
+        self.weights = weights
+        self.method = method
+        self.init_params = deepcopy(params)
+        Minimizer.__init__(self, model._residual, params, fcn_args=fcn_args,
+                           fcn_kws=fcn_kws, iter_cb=iter_cb,
+                           scale_covar=scale_covar, **fit_kws)
+
+    def fit(self, data=None, params=None, weights=None, method=None, **kwargs):
+        """perform fit for a Model, given data and params"""
+        if data is not None:
+            self.data = data
+        if params is not None:
+            self.params = params
+        if weights is not None:
+            self.weights = weights
+        if method is not None:
+            self.method = method
+        self.userargs = (self.data, self.weights)
+        self.userkws.update(kwargs)
+        self.init_params = deepcopy(self.params)
+        self.init_values = self.model._make_all_args(self.init_params)
+        self.init_fit    = self.model.eval(params=self.init_params, **self.userkws)
+
+        self.minimize(method=self.method)
+        self.best_fit = self.model.eval(params=self.params, **self.userkws)
+        self.best_values = self.model._make_all_args(self.params)
+
+    def eval(self, **kwargs):
+        self.userkws.update(kwargs)
+        return self.model.eval(params=self.params, **self.userkws)
+
+    def fit_report(self, modelpars=None, show_correl=True, min_correl=0.1):
+        "return fit report"
+        stats_report = fit_report(self, modelpars=modelpars,
+                                 show_correl=show_correl,
+                                 min_correl=min_correl)
+        buff = ['[[Model]]']
+        for x in self.model._reprstring(long=True):
+            buff.append('    %s' % x)
+        buff = '\n'.join(buff)
+        out = '%s\n%s' % (buff, stats_report)
         return out
