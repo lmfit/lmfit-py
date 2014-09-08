bc6327f1a384201c68bc4e72ada8534e455a81dd
Matt Newville <newville@cars.uchicago.edu>
2014-09-08 08:47:50 -0500

    add example for fit_report

---
+++ examples/doc_withreport.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+#<examples/doc_withreport.py>
+
+from __future__ import print_function
+from lmfit import Parameters, minimize, fit_report
+from numpy import random, linspace, pi, exp, sin, sign
+
+
+p_true = Parameters()
+p_true.add('amp', value=14.0)
+p_true.add('period', value=5.46)
+p_true.add('shift', value=0.123)
+p_true.add('decay', value=0.032)
+
+def residual(pars, x, data=None):
+    vals = pars.valuesdict()
+    amp =  vals['amp']
+    per =  vals['period']
+    shift = vals['shift']
+    decay = vals['decay']
+    
+    if abs(shift) > pi/2:
+        shift = shift - sign(shift)*pi
+    model = amp * sin(shift + x/per) * exp(-x*x*decay*decay)
+    if data is None:
+        return model
+    return (model - data)
+
+n = 1001
+xmin = 0.
+xmax = 250.0
+
+random.seed(0)
+
+noise = random.normal(scale=0.7215, size=n)
+x     = linspace(xmin, xmax, n)
+data  = residual(p_true, x) + noise
+
+fit_params = Parameters()
+fit_params.add('amp', value=13.0)
+fit_params.add('period', value=2)
+fit_params.add('shift', value=0.0)
+fit_params.add('decay', value=0.02)
+
+out = minimize(residual, fit_params, args=(x,), kws={'data':data})
+
+fit = residual(fit_params, x)
+print(fit_report(fit_params))
+
+#<end of examples/doc_withreport.py>
