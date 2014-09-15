83d297bb5347c0794364e772069a59ae80448500
Matt Newville <newville@cars.uchicago.edu>
2014-09-15 10:03:25 -0500

    need warnings imported, simplified fitter warnings

--- lmfit/ui/__init__.py
+++ lmfit/ui/__init__.py
@@ -1,5 +1,6 @@
 # These variables are used at the end of the module to decide
 # which BaseFitter subclass the Fitter will point to.
+import warnings
 
 has_ipython, has_matplotlib = False, False
 
@@ -15,19 +16,16 @@ try:
 except ImportError:
     pass
 else:
+    _ipy_msg1 = "lmfit.Fitter will use basic mode, not IPython: need IPython2."
+    _ipy_msg2 = "lmfit.Fitter will use basic mode, not IPython: could not get IPython version"
     try:
         if IPython.release.version_info[0] < 2:
-            warnings.warn("IPython versions before 2.0 are not supported. "
-                          "Fitter will operate in basic mode, as it would "
-                          "in a plain python interpreter.")
+            warnings.warn(_ipy_msg1)
         else:
             # has_ipython = iPython installed and we are in an IPython session.
             has_ipython = IPython.get_ipython() is not None
     except Exception as e:
-        warnings.warn("An error occurred while trying to detect IPython. "
-                      "Fitter will operate in basic mode. The error is: "
-                      "{0}".format(e))
-
+        warnings.warn(_ipy_msg2)
 
 from .basefitter import BaseFitter
 Fitter = BaseFitter
