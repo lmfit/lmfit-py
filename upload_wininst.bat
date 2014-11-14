0e2ad768ce26589b95f61e92ba4bac60cbba2bc3
Matt Newville <newville@cars.uchicago.edu>
2014-11-14 12:05:24 -0600

    use py3.4 in upload script

--- upload_wininst.bat
+++ upload_wininst.bat
@@ -2,17 +2,15 @@ REM
 REM  %HOME%\.pypirc must be setup with PyPI info.
 REM
 SET HOME=M:\
-SET PATH=C:\Python26;%PATH%
-python setup.py install
-python setup.py bdist_wininst --target-version=2.6 upload
 
 SET PATH=C:\Python27;%PATH%
 python setup.py install
 python setup.py bdist_wininst --target-version=2.7 upload
 
-SET PATH=C:\Python32;%PATH%
+
+SET PATH=C:\Python34;%PATH%
 python setup.py install
-python setup.py bdist_wininst --target-version=3.2 upload
+python setup.py bdist_wininst --target-version=3.4 upload
 
 
 
