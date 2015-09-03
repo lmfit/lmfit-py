BUG: emcee was scaling initialising position from internal parameter value

lmfit/minimizer.py
@@ -575,7 +575,7 @@ class Minimizer(object):
                                       ' the emcee method')
 
         result = self.prepare_fit(params=params)
-        vars   = result.init_vals
+        vars = result.init_vals
         params = result.params
 
         # Removing internal parameter scaling. We could possibly keep it,
@@ -583,7 +583,7 @@ class Minimizer(object):
         bounds_varying = []
         for i, par in enumerate(params):
             param = params[par]
-            vars[i] = param.from_internal(param.value)
+            vars[i] = param.value
             param.from_internal = lambda val: val
             lb, ub = param.min, param.max
             if lb is None or lb is np.nan:
@@ -591,7 +591,6 @@ class Minimizer(object):
             if ub is None or ub is np.nan:
                 ub = np.inf
             bounds_varying.append((lb, ub))
-
         bounds_varying = np.array(bounds_varying)
 
         self.nvarys = len(result.var_names)
@@ -625,12 +624,12 @@ class Minimizer(object):
 
         if ntemps > 1:
             # jitter the starting position by scaled Gaussian noise (1% level)
-            p0 = 1 + np.random.randn(ntemps, nwalkers, self.nvarys) * 1.e-2
+            p0 = 1 + np.random.randn(ntemps, nwalkers, self.nvarys) * 1.e-4
             p0 *= vars
             sampler = emcee.PTSampler(ntemps, nwalkers, self.nvarys, lnlike,
                                       lnprior)
         else:
-            p0 = 1 + np.random.randn(nwalkers, self.nvarys) * 1.e-2
+            p0 = 1 + np.random.randn(nwalkers, self.nvarys) * 1.e-4
             p0 *= vars
             sampler = emcee.EnsembleSampler(nwalkers, self.nvarys, lnprob)
 

tests/test_nose.py
@@ -395,17 +395,17 @@ class CommonMinimizerTest(unittest.TestCase):
                                    self.p_true.values()):
             check_wo_stderr(para, true_para.value, sig=sig)
 
-    # @decorators.slow
-    # def test_emcee(self):
-    #     # test emcee
-    #     if not HAS_EMCEE:
-    #         return True
-    #
-    #     np.random.seed(123456)
-    #     out, chains = self.mini.emcee(nwalkers=100, steps=500,
-    #                                   burn=200, thin=20)
-    #
-    #     check_paras(out.params, self.p_true, sig=3)
+    @decorators.slow
+    def test_emcee(self):
+        # test emcee
+        if not HAS_EMCEE:
+            return True
+
+        np.random.seed(123456)
+        out = self.mini.emcee(nwalkers=100, steps=500,
+                                      burn=200, thin=20)
+
+        check_paras(out.params, self.p_true, sig=3)
 
     @decorators.slow
     def test_emcee_PT(self):
@@ -414,25 +414,25 @@ class CommonMinimizerTest(unittest.TestCase):
             return True
 
         np.random.seed(123456)
-        out, chains = self.mini.emcee(ntemps=4, nwalkers=100, steps=500,
+        out = self.mini.emcee(ntemps=4, nwalkers=100, steps=500,
                                       burn=300, thin=10)
 
         check_paras(out.params, self.p_true, sig=3)
 
-    # @decorators.slow
-    # def test_emcee_partial_bounds(self):
-    #     # test mcmc with partial bounds
-    #     if not HAS_EMCEE:
-    #         return True
-    #
-    #     np.random.seed(123456)
-    #     # test mcmc output vs lm, some parameters not bounded
-    #     self.fit_params['amp'].max = None
-    #     # self.fit_params['amp'].min = None
-    #     out, chains = self.mini.emcee(nwalkers=150, steps=600,
-    #                                   burn=300, thin=10)
-    #
-    #     check_paras(out.params, self.p_true, sig=5)
+    @decorators.slow
+    def test_emcee_partial_bounds(self):
+        # test mcmc with partial bounds
+        if not HAS_EMCEE:
+            return True
+
+        np.random.seed(123456)
+        # test mcmc output vs lm, some parameters not bounded
+        self.fit_params['amp'].max = None
+        # self.fit_params['amp'].min = None
+        out = self.mini.emcee(nwalkers=150, steps=600,
+                                      burn=300, thin=10)
+
+        check_paras(out.params, self.p_true, sig=5)
 
     def test_emcee_output(self):
         # test mcmc output
@@ -443,11 +443,11 @@ class CommonMinimizerTest(unittest.TestCase):
         except ImportError:
             return True
 
-        out, chain = self.mini.emcee(nwalkers=10, steps=10)
+        out = self.mini.emcee(nwalkers=10, steps=10)
         assert_(isinstance(out, MinimizerResult))
-        assert_(isinstance(chain, DataFrame))
+        assert_(isinstance(out.chain, DataFrame))
         # check that we can access the chains via parameter name
-        assert_(chain['amp'].shape[0] == 100)
+        assert_(out.chain['amp'].shape[0] == 100)
         assert_(out.errorbars == True)
         assert_(np.isfinite(out.params['amp'].correl['period']))
 