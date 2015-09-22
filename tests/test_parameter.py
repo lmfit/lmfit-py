6f3e907f014f839014ebf434ff36d0d5186e5912
Andrew Nelson <andyfaff@gmail.com>
2015-09-22 16:39:32 +1000

    TST: added a Parameters pickle test

--- tests/test_parameter.py
+++ tests/test_parameter.py
@@ -23,10 +23,18 @@ class TestFitter(unittest.TestCase):
         # test that we can pickle a Parameters object
         p = Parameters()
         p.add('a', 10, True, 0, 1)
+        p.add('b', 10, True, 0, 100, 'a * 2')
 
         pkl = pickle.dumps(p, -1)
         q = pickle.loads(pkl)
 
+        assert_(p == q)
+        assert_(not p is q)
+
+        # now test if the asteval machinery survived
+        p['a'].value = 5
+        assert_(p != q)
+        q['a'].value = 5
         assert_(p == q)
 
 
