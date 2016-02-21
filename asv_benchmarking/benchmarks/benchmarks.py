deccec4f5ae81da5550b7459f86c2f871198e545
Matthew Newville <newville@cars.uchicago.edu>
2016-02-21 10:30:50 -0600

    add benchmark that uses conf_interval()

--- asv_benchmarking/benchmarks/benchmarks.py
+++ asv_benchmarking/benchmarks/benchmarks.py
@@ -4,7 +4,8 @@ import numpy as np
 import time
 import cProfile, pstats
 from subprocess import Popen, PIPE
-from lmfit import minimize, Parameters,  __version__
+from lmfit import (minimize, Parameters, Minimizer,  __version__,
+                   conf_interval)
 
 
 def obj_func(params, x, data):
@@ -24,10 +25,9 @@ class MinimizeSuite:
         pass
 
 
-
     def time_minimize(self):
-        x = np.linspace(0, 15, 601)
         np.random.seed(201)
+        x = np.linspace(0, 15, 601)
 
         data = (5. * np.sin(2 * x - 0.1) * np.exp(-x*x*0.025) +
                 np.random.normal(size=len(x), scale=0.3) )
@@ -38,3 +38,21 @@ class MinimizeSuite:
         params.add('omega', value= 1.0, min=0, max=10)
 
         out = minimize(obj_func, params, args=(x, data))
+
+    def time_confinterval(self):
+        np.random.seed(0)
+        x = np.linspace(0.3,10,100)
+        y = 1/(0.1*x)+2+0.1*np.random.randn(x.size)
+
+        p = Parameters()
+        p.add_many(('a', 0.1), ('b', 1))
+
+        def residual(p):
+            a = p['a'].value
+            b = p['b'].value
+
+            return 1/(a*x)+b-y
+
+        minimizer = Minimizer(residual, p)
+        out = minimizer.leastsq()
+        ci = conf_interval(minimizer, out)
