6d59644211cb981d7736618d5c6d328796969f15
Matthew Newville <newville@cars.uchicago.edu>
2017-03-04 21:06:57 -0600

    some sphinx config updates

--- doc/sphinx/ext_mathjax.py
+++ doc/sphinx/ext_mathjax.py
@@ -1,10 +1,9 @@
 # sphinx extensions for mathjax
+
 extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.todo',
               'sphinx.ext.coverage',
               'sphinx.ext.intersphinx',
-              'numpydoc']
-mathjax = 'sphinx.ext.mathjax'
-pngmath = 'sphinx.ext.pngmath'
-
-extensions.append(mathjax)
+              'sphinx.ext.extlinks',
+              'sphinx.ext.napoleon',
+              'sphinx.ext.mathjax']
