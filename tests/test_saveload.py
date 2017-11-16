987eb5709b5274ef9625363ac4fd8419ff52653b
Matthew Newville <newville@cars.uchicago.edu>
2017-11-16 14:58:52 -0600

    updates to saveload test

--- tests/test_saveload.py
+++ tests/test_saveload.py
@@ -48,6 +48,26 @@ def create_model_params(x, y):
     model = gauss1 + gauss2 + exp_mod
     return model, params
 
+def check_fit_results(result):
+    assert(result.nvarys == 8)
+    assert_between(result.chisqr,  1000, 1500)
+    assert_between(result.aic,  400, 450)
+
+    pars = result.params
+    assert_param_between(pars['exp_decay'], 90, 92)
+    assert_param_between(pars['exp_amplitude'], 98, 101)
+    assert_param_between(pars['g1_sigma'], 16, 17)
+    assert_param_between(pars['g1_center'], 106, 109)
+    assert_param_between(pars['g1_amplitude'], 4100, 4500)
+    assert_param_between(pars['g1_fwhm'], 38, 42)
+    assert_param_between(pars['g1_height'], 100, 103)
+
+    assert_param_between(pars['g2_sigma'],    10, 15)
+    assert_param_between(pars['g2_center'],  150, 160)
+    assert_param_between(pars['g2_amplitude'], 2100, 2900)
+    assert_param_between(pars['g2_fwhm'], 30, 34)
+    assert_param_between(pars['g2_height'], 70, 75)
+
 def test_save_model():
     x, y = get_data()
     model, params = create_model_params(x, y)
@@ -59,6 +79,25 @@ def test_save_model():
     with open(SAVE_MODEL, 'r') as fh:
         text = fh.read()
     assert_between(len(text), 1000, 1500)
+
+def test_load_model():
+    x, y = get_data()
+    model = load_model(SAVE_MODEL)
+    params = model.make_params()
+
+    params['exp_decay'].set(100)
+    params['exp_amplitude'].set(100)
+    params['g1_center'].set(105, min=75, max=125)
+    params['g1_sigma'].set(15, min=3)
+    params['g1_amplitude'].set(2000, min=10)
+
+    params['g2_center'].set(155, min=125, max=175)
+    params['g2_sigma'].set(15, min=3)
+    params['g2_amplitude'].set(2000, min=10)
+
+    result = model.fit(y,  params, x=x)
+    check_fit_results(result)
+
 
 def test_save_modelresult():
     x, y = get_data()
@@ -88,28 +127,10 @@ def test_save_modelresult():
 
 def test_load_modelresult():
     result = load_modelresult(SAVE_MODELRESULT)
-
-    assert(result.nvarys == 8)
-    assert_between(result.chisqr,  1000, 1500)
-    assert_between(result.aic,  400, 450)
-
-    pars = result.params
-    assert_param_between(pars['exp_decay'], 90, 92)
-    assert_param_between(pars['exp_amplitude'], 98, 101)
-    assert_param_between(pars['g1_sigma'], 16, 17)
-    assert_param_between(pars['g1_center'], 106, 109)
-    assert_param_between(pars['g1_amplitude'], 4100, 4500)
-    assert_param_between(pars['g1_fwhm'], 38, 42)
-    assert_param_between(pars['g1_height'], 100, 103)
-
-    assert_param_between(pars['g2_sigma'],    10, 15)
-    assert_param_between(pars['g2_center'],  150, 160)
-    assert_param_between(pars['g2_amplitude'], 2100, 2900)
-    assert_param_between(pars['g2_fwhm'], 30, 34)
-    assert_param_between(pars['g2_height'], 70, 75)
-
+    check_fit_results(result)
 
 if __name__ == '__main__':
     test_save_model()
+    test_load_model()
     test_save_modelresult()
     test_load_modelresult()
