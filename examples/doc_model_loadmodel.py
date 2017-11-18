92733f89dcb023b946d14411c4575f0d38a10342
Matthew Newville <newville@cars.uchicago.edu>
2017-11-18 16:44:19 -0600

    rename doc examples to correctly state chapter name

---
+++ examples/doc_model_loadmodel.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+#<examples/doc_load_model.py>
+import numpy as np
+import matplotlib.pyplot as plt
+from lmfit.model import Model, save_model, load_model
+
+def mysine(x, amp, freq, shift):
+    return amp * np.sin(x*freq + shift)
+
+data = np.loadtxt('sinedata.dat')
+x = data[:, 0]
+y = data[:, 1]
+
+model = load_model('sinemodel.sav', funcdefs={'mysine': mysine})
+params = model.make_params(amp=3, freq=0.52, shift=0)
+params['shift'].max = 1
+params['shift'].min = -1
+params['amp'].min = 0.0
+
+result = model.fit(y,  params, x=x)
+print(result.fit_report())
+
+plt.plot(x, y,         'bo')
+plt.plot(x, result.best_fit, 'r-')
+plt.show()
+
+#<end examples/doc_load_model.py>
