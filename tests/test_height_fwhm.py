702c4e51bbec1f5aa80d68626dd7e3264264be71
Austin Fox <openafox@gmail.com>
2017-11-30 00:54:44 -0800

    Fix for Travis

--- tests/test_height_fwhm.py
+++ tests/test_height_fwhm.py
@@ -6,19 +6,34 @@ import inspect
 import numpy as np
 from scipy.stats import norm
 from scipy.optimize import fsolve
-from matplotlib import pyplot as plt
+import sys
 
 
-def test_height_fwhm(x, y, lineshape, model, plot=False, report=False):
+# Turn off plotting if run by nosetests.
+WITHPLOT = True
+for arg in sys.argv:
+    if 'nose' in arg or 'pytest' in arg:
+        WITHPLOT = False
+
+if WITHPLOT:
+    try:
+        import matplotlib
+        import pylab
+    except ImportError:
+        WITHPLOT = False
+
+
+def check_height_fwhm(x, y, lineshape, model, with_plot=True, report=False):
     """Check height and fwhm parameters"""
+    with_plot = with_plot and WITHPLOT
     pars = model.guess(y, x=x)
     out = model.fit(y, pars, x=x)
     if report:
         print(out.fit_report())
-    if plot:
-        fig = plt.figure()
+    if with_plot:
+        fig = pylab.figure()
         out.plot(fig=fig)
-        plt.show()
+        pylab.show()
 
     # account for functions whose centers are not mu
     mu = out.params['center'].value
@@ -55,8 +70,7 @@ def test_height_fwhm(x, y, lineshape, model, plot=False, report=False):
 
     print(model._name, 'OK')
 
-
-if __name__ == '__main__':
+def test_peak_like():
     # mu = 0
     # variance = 1.0
     # sigma = np.sqrt(variance)
@@ -65,16 +79,19 @@ if __name__ == '__main__':
     data = np.loadtxt('../examples/test_peak.dat')
     x = data[:, 0]
     y = data[:, 1]
-    test_height_fwhm(x, y, lineshapes.voigt, models.VoigtModel())
-    test_height_fwhm(x, y, lineshapes.pvoigt, models.PseudoVoigtModel())
-    test_height_fwhm(x, y, lineshapes.pearson7, models.Pearson7Model())
-    test_height_fwhm(x, y, lineshapes.moffat, models.MoffatModel())
-    test_height_fwhm(x, y, lineshapes.students_t, models.StudentsTModel())
-    test_height_fwhm(x, y, lineshapes.breit_wigner, models.BreitWignerModel())
-    test_height_fwhm(x, y, lineshapes.damped_oscillator, models.DampedOscillatorModel())
-    test_height_fwhm(x, y, lineshapes.dho, models.DampedHarmonicOscillatorModel())
-    test_height_fwhm(x, y, lineshapes.expgaussian, models.ExponentialGaussianModel())
-    test_height_fwhm(x, y, lineshapes.skewed_gaussian, models.SkewedGaussianModel())
-    test_height_fwhm(x, y, lineshapes.donaich, models.DonaichModel())
+    check_height_fwhm(x, y, lineshapes.voigt, models.VoigtModel())
+    check_height_fwhm(x, y, lineshapes.pvoigt, models.PseudoVoigtModel())
+    check_height_fwhm(x, y, lineshapes.pearson7, models.Pearson7Model())
+    check_height_fwhm(x, y, lineshapes.moffat, models.MoffatModel())
+    check_height_fwhm(x, y, lineshapes.students_t, models.StudentsTModel())
+    check_height_fwhm(x, y, lineshapes.breit_wigner, models.BreitWignerModel())
+    check_height_fwhm(x, y, lineshapes.damped_oscillator, models.DampedOscillatorModel())
+    check_height_fwhm(x, y, lineshapes.dho, models.DampedHarmonicOscillatorModel())
+    check_height_fwhm(x, y, lineshapes.expgaussian, models.ExponentialGaussianModel())
+    check_height_fwhm(x, y, lineshapes.skewed_gaussian, models.SkewedGaussianModel())
+    check_height_fwhm(x, y, lineshapes.donaich, models.DonaichModel())
     x=x-9 # Lognormal will only fit peaks with centers < 1
-    test_height_fwhm(x, y, lineshapes.lognormal, models.LognormalModel())
+    check_height_fwhm(x, y, lineshapes.lognormal, models.LognormalModel())
+
+if __name__ == '__main__':
+    test_peak_like()
