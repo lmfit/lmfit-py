9f509929f8f5c864c78fa2799d215fa94a8e1042
Matthew Newville <newville@cars.uchicago.edu>
2018-03-20 19:43:06 -0500

    update doc

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: '',
-    VERSION: '0.9.9+1.g1e90b69.dirty',
+    VERSION: '0.9.9',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
