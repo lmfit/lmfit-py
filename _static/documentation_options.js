66ef0502fad1b4ed1bfe8ebec7b8f44d02144162
Matthew Newville <newville@cars.uchicago.edu>
2018-03-20 19:29:51 -0500

    update doc

---
+++ _static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: '',
+    VERSION: '0.9.9+1.g1e90b69.dirty',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt'
+};
