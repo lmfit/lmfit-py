b48e6cc8c76d0e0369921f12d7a9582b1a200da5
reneeotten <reneeotten@users.noreply.github.com>
2018-05-12 15:40:33 -0400

    DOC: set random.seed for reproducible examples in the documentation.
    Update the fit_report and figures in the documentation accordingly.

--- examples/doc_builtinmodels_stepmodel.py
+++ examples/doc_builtinmodels_stepmodel.py
@@ -10,6 +10,7 @@ x = np.linspace(0, 10, 201)
 y = np.ones_like(x)
 y[:48] = 0.0
 y[48:77] = np.arange(77-48)/(77.0-48)
+np.random.seed(0)
 y = 110.2 * (y + 9e-3*np.random.randn(len(x))) + 12.0 + 2.22*x
 
 step_mod = StepModel(form='erf', prefix='step_')
