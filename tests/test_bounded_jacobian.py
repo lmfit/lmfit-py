a70ef7e949d17ecaa49a11f726f2eef88979dfd4
Marcel Stimberg <marcel.stimberg@inserm.fr>
2020-11-16 16:40:19 +0100

    Fix use of callable Jacobian for scalar methods

--- tests/test_bounded_jacobian.py
+++ tests/test_bounded_jacobian.py
@@ -35,3 +35,36 @@ def test_bounded_jacobian():
     assert_paramval(out1.params['x0'], 1.2243, tol=0.02)
     assert_paramval(out1.params['x1'], 1.5000, tol=0.02)
     assert jac_count > 5
+
+
+def test_bounded_jacobian_CG():
+    pars = Parameters()
+    pars.add('x0', value=2.0)
+    pars.add('x1', value=2.0, min=1.5)
+
+    global jac_count
+
+    jac_count = 0
+
+    def resid(params):
+        x0 = params['x0']
+        x1 = params['x1']
+        return np.array([10 * (x1 - x0*x0), 1-x0])
+
+    def jac(params):
+        global jac_count
+        jac_count += 1
+        x0 = params['x0']
+        # Jacobian of the *error*, i.e. the summed squared residuals
+        return 2 * np.sum(np.array([[-20 * x0, 10],
+                                    [-1, 0]]).T * resid(params), axis=1)
+
+    out0 = minimize(resid, pars, method='CG')
+    assert_paramval(out0.params['x0'], 1.2243, tol=0.02)
+    assert_paramval(out0.params['x1'], 1.5000, tol=0.02)
+    assert jac_count == 0
+
+    out1 = minimize(resid, pars, method='CG', Dfun=jac)
+    assert_paramval(out1.params['x0'], 1.2243, tol=0.02)
+    assert_paramval(out1.params['x1'], 1.5000, tol=0.02)
+    assert jac_count > 5
