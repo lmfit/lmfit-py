76db16a9b561b1d8c6698e3f7366356bc06b61a8
reneeotten <reneeotten@users.noreply.github.com>
2021-01-01 00:03:46 -0500

    MAINT: update copyright year

--- lmfit/__init__.py
+++ lmfit/__init__.py
@@ -30,7 +30,7 @@ useful enhancements, including:
   * Many built-in models for common lineshapes are included and ready
     to use.
 
-Copyright (c) 2020 Lmfit Developers ; BSD-3 license ; see LICENSE
+Copyright (c) 2021 Lmfit Developers ; BSD-3 license ; see LICENSE
 
 """
 from asteval import Interpreter
