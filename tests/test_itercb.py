8ad7e282eb4cceec4b3fc07a9b017c6909fc294d
reneeotten <reneeotten@users.noreply.github.com>
2021-10-09 18:07:27 -0400

    TST: add test for _abort issue
    For reasons that I don't fully understand of as yet, all scalar_minimizers
    currently (x)fail the test. We treat them differently in that we
    explictly set ``self._abort = False`` on line 1039; we don't do that in
    any of the other functions.

--- tests/test_itercb.py
+++ tests/test_itercb.py
@@ -4,6 +4,7 @@ import numpy as np
 import pytest
 
 from lmfit.lineshapes import gaussian
+from lmfit.minimizer import Minimizer
 from lmfit.models import GaussianModel, LinearModel
 
 try:
@@ -18,6 +19,15 @@ x = np.linspace(0, 20, 401)
 y = gaussian(x, amplitude=24.56, center=7.6543, sigma=1.23)
 y -= 0.20*x + 3.333 + np.random.normal(scale=0.23, size=len(x))
 mod = GaussianModel(prefix='peak_') + LinearModel(prefix='bkg_')
+
+
+def residual(pars, x, data):
+    parvals = pars.valuesdict()
+    gauss = gaussian(x, parvals['peak_amplitude'], parvals['peak_center'],
+                     parvals['peak_sigma'])
+    linear = parvals['bkg_slope']*x + parvals['bkg_intercept']
+    return data - gauss - linear
+
 
 pars = mod.make_params(peak_amplitude=21.0, peak_center=7.0,
                        peak_sigma=2.0, bkg_intercept=2, bkg_slope=0.0)
@@ -35,11 +45,13 @@ def per_iteration(pars, iteration, resid, *args, **kws):
     return iteration == 23
 
 
+fitmethods = ['ampgo', 'brute', 'basinhopping', 'differential_evolution',
+              'leastsq', 'least_squares', 'nelder', 'shgo', 'dual_annealing']
+
+
 @pytest.mark.parametrize("calc_covar", calc_covar_options)
-@pytest.mark.parametrize("method", ['ampgo', 'brute', 'basinhopping',
-                                    'differential_evolution', 'leastsq',
-                                    'least_squares', 'nelder'])
-def test_itercb(method, calc_covar):
+@pytest.mark.parametrize("method", fitmethods)
+def test_itercb_model_class(method, calc_covar):
     """Test the iteration callback for all solvers."""
     out = mod.fit(y, pars, x=x, method=method, iter_cb=per_iteration,
                   calc_covar=calc_covar)
@@ -48,3 +60,71 @@ def test_itercb(method, calc_covar):
     assert out.aborted
     assert not out.errorbars
     assert not out.success
+
+
+@pytest.mark.parametrize("calc_covar", calc_covar_options)
+@pytest.mark.parametrize("method", fitmethods)
+def test_itercb_minimizer_class(method, calc_covar):
+    """Test the iteration callback for all solvers."""
+    if method in ('nelder', 'differential_evolution'):
+        pytest.xfail("scalar_minimizers behave differently, but shouldn't!!")
+
+    mini = Minimizer(residual, pars, fcn_args=(x, y), iter_cb=per_iteration,
+                     calc_covar=calc_covar)
+    out = mini.minimize(method=method)
+
+    assert out.nfev == 23
+    assert out.aborted
+    assert not out.errorbars
+    assert not out.success
+    assert mini._abort
+
+
+fitmethods = ['leastsq', 'least_squares']
+
+
+@pytest.mark.parametrize("method", fitmethods)
+def test_itercb_reset_abort(method):
+    """Regression test for GH Issue #756.
+
+    Make sure that ``self._abort`` is reset to ``False`` at the start of each
+    fit.
+
+    """
+    if method in ('nelder', 'differential_evolution'):
+        pytest.xfail("scalar_minimizers behave differently, but shouldn't!!")
+
+    must_stop = True
+
+    def callback(*args, **kwargs):
+        return must_stop
+
+    # perform minimization with ``iter_cb``
+    out_model = mod.fit(y, pars, x=x, method=method, iter_cb=callback)
+
+    mini = Minimizer(residual, pars, fcn_args=(x, y), iter_cb=callback)
+    out_minimizer = mini.minimize(method=method)
+
+    assert out_model.aborted is must_stop
+    assert out_model.errorbars is not must_stop
+    assert out_model.success is not must_stop
+    assert out_minimizer.aborted is must_stop
+    assert out_minimizer.errorbars is not must_stop
+    assert out_minimizer.success is not must_stop
+    assert mini._abort is must_stop
+
+    # perform another minimization now without ``iter_cb``
+    must_stop = False
+    out_minimizer_no_callback = mini.minimize(method=method)
+    assert out_minimizer_no_callback.aborted is must_stop
+    assert out_minimizer_no_callback.errorbars is not must_stop
+    assert out_minimizer_no_callback.success is not must_stop
+    assert mini._abort is must_stop
+
+    # reset to mini._abort to False and call the optimization method directly
+    func = getattr(mini, method)
+    out_no_callback = func()
+    assert out_no_callback.aborted is must_stop
+    assert out_no_callback.errorbars is not must_stop
+    assert out_no_callback.success is not must_stop
+    assert mini._abort is must_stop
