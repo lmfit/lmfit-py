99b13a4f45bacdbafbfe8a3d5b0b0448c515e0f1
Matthew Newville <newville@cars.uchicago.edu>
2022-11-20 18:32:57 -0600

    relax assertion of modelresult size

--- tests/test_model_saveload.py
+++ tests/test_model_saveload.py
@@ -144,7 +144,7 @@ def test_save_load_modelresult(dill):
     text = ''
     with open(SAVE_MODELRESULT) as fh:
         text = fh.read()
-    assert 18000 < len(text) < 21000  # depending on whether dill is present
+    assert 12000 < len(text) < 60000  # depending on whether dill is present
 
     # load the saved ModelResult from file and compare results
     result_saved = load_modelresult(SAVE_MODELRESULT)
