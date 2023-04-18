d9ef7c572e1cc2d28a242dacd701c55e2c812e05
reneeotten <reneeotten@users.noreply.github.com>
2023-04-17 21:06:05 -0400

    DOC: remove only the spurious link to 'Examples gallery'

--- doc/filter_spurious_link_from_html.py
+++ doc/filter_spurious_link_from_html.py
@@ -15,7 +15,7 @@ doc_dir = Path(__file__).parent.absolute()
 examples_html_dir = doc_dir.parent / 'doc/_build/html/examples'
 files = examples_html_dir.glob('*.html')
 
-link = b'Examples gallery'
+link = b'accesskey="U"'
 
 for fn in files:
     with open(fn, 'rb') as file_in:
