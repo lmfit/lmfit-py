3a66f613d00d1762d0aac8c1354500557df79060
Matt Newville <newville@cars.uchicago.edu>
2026-07-15 10:33:34 -0500

    add example for loading modelresult from text

---
+++ examples/doc_model_loadmodelresult3.py
@@ -0,0 +1,23 @@
+# <examples/doc_model_loadmodelresult2.py>
+import os
+import sys
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from lmfit.model import load_modelresult
+
+if not os.path.exists('nistgauss_modelresult.sav'):
+    os.system(f"{sys.executable} doc_model_savemodelresult2.py")
+
+dat = np.loadtxt('NIST_Gauss2.dat')
+x = dat[:, 1]
+y = dat[:, 0]
+
+result = load_modelresult(open('nistgauss_modelresult.sav').read())
+print(result.fit_report())
+
+plt.plot(x, y, 'o')
+plt.plot(x, result.best_fit, '-')
+plt.show()
+# <end examples/doc_model_loadmodelresult2.py>
